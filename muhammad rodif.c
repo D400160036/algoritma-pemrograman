af3dffa1445f27abb9bbd1b415983fde7148b7da
D400160036 <D400160036@users.noreply.github.com>
2016-10-10 09:26:50 +0700

    Add files via upload

---
+++ muhammad rodif.c
@@ -0,0 +1,24 @@
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+clrscr();
+float a,b,c,x1,x2;
+ int D;
+ D=pow(b,2)-4*a*c;
+if (D<0)  x1=x2; "bilangan imajiner";
+}
+else if (D>0)
+{
+    x1=(-b+(sqrt pow(b,2)-4*a*c))/(2*a);
+    x2=(-b-(sqrt pow(b,2)-4*a*c))/(2*a);
+}
+
+{
+(x1,x2);
+return 0;
+}
+
+
